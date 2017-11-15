fca7722c4f6d138739278b6824e29d3721a068a1
mbarutcu97 <mbarutcu97@gmail.com>
2017-11-15 18:09:08 +0100

    Alleen open vragen

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Quiz
 {
-    // Main
+    // Alleen open vragen
     class Program
     {
         static void Main(string[] args)
         {
+            List<Vraag> Vragen = new List<Vraag>
+            {
+                new Vraag {Tekst = "Welke kleur heeft een aardbei?", Antwoord = "Rood", Moeilijkheidsgraad = 1, Categorie = "Algemeen"},
+                new Vraag {Tekst = "Wat is de hoofdstad van Nederland?", Antwoord = "Amsterdam", Moeilijkheidsgraad = 2, Categorie = "Algemeen"},
+                new Vraag {Tekst = "Hoeveel snaren heeft een mandoline?", Antwoord = "8", Moeilijkheidsgraad = 3, Categorie = "Algemeen"},
+                new Vraag {Tekst = "Hoe noem je een mannelijke schaap?", Antwoord = "Ram", Moeilijkheidsgraad = 1, Categorie = "Natuur"},
+                new Vraag {Tekst = "In welk werelddeel komt de Hyena voor?", Antwoord = "Afrika", Moeilijkheidsgraad = 2, Categorie = "Natuur"},
+                new Vraag {Tekst = "Welke zoogdier wordt met uitsterven bedreigd?", Antwoord = "Reuzenpanda", Moeilijkheidsgraad = 3, Categorie = "Natuur"}
+
+             };
+            var GesorteerdeVragen = Vragen.OrderBy(a => a.Moeilijkheidsgraad);
+            int MGraad = 0;
+            String categorie = "";
+
+            Console.WriteLine("Kiezen uit MOEILIJKHEIDSGRAAD (type M)" + "\n" + "Kiezen uit CATEGORIE (type C)");
+            String keuze = Console.ReadLine();
+            if (keuze.Equals("M"))
+            {
+                Console.WriteLine("Kies MOEILIJKHEIDSGRAAD (1, 2, 3)");
+                String MGraadString = Console.ReadLine();
+                Int32.TryParse(MGraadString, out MGraad);
+
+            } else if (keuze.Equals("C"))
+            {
+                Console.WriteLine("Kies CATEGORIE (Algemeen, Natuur)");
+                categorie = Console.ReadLine();
+            }
+
+            GesorteerdeVragen.Where(x => x.Moeilijkheidsgraad == MGraad || x.Categorie.Equals(categorie)).ToList().ForEach(
+                  x =>
+                  {
+                      x.Display();
+                      Console.Write("Uw antwoord: ");
+                      String antw = Console.ReadLine();
+                      Console.WriteLine(x.CheckAntwoord(antw));
+                  }
+                  );
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Quiz/Quiz/Vraag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class Vraag
+    {
+        public String Tekst { get; set; }
+        public String Antwoord { get; set; }
+        public int Moeilijkheidsgraad { get; set; }
+        public String Categorie { get; set; }
+       
+        public Vraag()
+        {
+        }
+
+        public Boolean CheckAntwoord(String antwoord)
+        {
+            return this.Antwoord.Equals(antwoord);
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Tekst);
+        }
+    }
+}
